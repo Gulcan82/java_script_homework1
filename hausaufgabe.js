898cac04b4225054b64cc789ad5f0fdf79f5aafe
Gülcan Kamat <gulcankamat@gmail.com>
2024-03-15 16:52:12 +0100

    Adding the homework from today.

---
+++ hausaufgabe.js
@@ -0,0 +1,62 @@
+let lieblingsBuch = "Titanic";
+console.log(lieblingsBuch);
+
+let anzahlGeschwister = 4;
+console.log(anzahlGeschwister);
+
+let hatFuehrerschein = "Ja";
+console.log(hatFuehrerschein);
+
+let momentaneStimmung = "null";
+console.log(momentaneStimmung);
+
+let myUndefinedVariable;
+console.log(myUndefinedVariable);
+
+let lieblingsFarbe = "grün";
+console.log(lieblingsFarbe);
+
+let wohnflaeche = 85;
+console.log(wohnflaeche);
+
+let magKaffee = "true";
+console.log(magKaffee);
+
+let aktuellesProjekt = "null"
+console.log(aktuellesProjekt);
+
+let letztesAbendessen;
+console.log(letztesAbendessen);
+
+let geburtstadt = "Berlin";
+console.log(geburtstadt);
+
+let kaffeetassenProTag = 2;
+console.log(kaffeetassenProTag);
+
+let istVegetarier = "false"
+console.log(istVegetarier);
+
+let zukunftsWunsch = "null";
+console.log(zukunftsWunsch);
+
+let lieblingsSportart;
+console.log(lieblingsSportart);
+
+let lieblingsFilm = "Avatar";
+console.log(lieblingsFilm);
+
+let hoeheIncm = 165;
+console.log(hoeheIncm);
+
+let hatHaustier = "true";
+console.log(hatHaustier);
+
+let mittagessenIdee = "null";
+console.log(mittagessenIdee);
+
+let lieblingsMusikband;
+console.log(lieblingsMusikband);
+
+
+
